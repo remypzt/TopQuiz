92431c6f974c8162922877e798c973b33262b0f1
Votre nom ou pseudo <remy.pouzet@laposte.net>
2019-06-19 16:07:07 +0200

    try to make final exercice

--- app/src/main/java/remy/pouzet/topquiz/com/controller/RankingActivity.java
+++ app/src/main/java/remy/pouzet/topquiz/com/controller/RankingActivity.java
@@ -9,9 +9,10 @@ import android.widget.TextView;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
 
 import remy.pouzet.topquiz.com.R;
-import remy.pouzet.topquiz.com.model.Players;
 
 public class RankingActivity extends AppCompatActivity
 {
@@ -29,6 +30,15 @@ public class RankingActivity extends AppCompatActivity
 
     public static final String PREF_PLAYERS_LIST = "PREF_PLAYERS_LIST";
 
+    public TextView getTextView1()
+    {
+        return mTextView1;
+    }
+
+    public void setTextView1(TextView textView1)
+    {
+        mTextView1 = textView1;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,18 +46,6 @@ public class RankingActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
 
         System.out.println("RankingActivity::onCreate()");
-
-        mPreferences = getPreferences(MODE_PRIVATE);
-
-        String  jsonPlayersList = mPreferences.getString(PREF_PLAYERS_LIST, null);
-
-        Gson gson = new Gson();
-        ArrayList playersList = gson.fromJson(jsonPlayersList, Players.class);
-
-
-
-
-        setContentView(R.layout.activity_ranking);
 
         mTextView1 = findViewById(R.id.textView);
         mTextView2 = findViewById(R.id.textView2);
@@ -57,13 +55,19 @@ public class RankingActivity extends AppCompatActivity
         mButton1 = findViewById(R.id.button);
         mButton2 = findViewById(R.id.button2);
 
+        mPreferences = getPreferences(MODE_PRIVATE);
+
+        String jsonPlayersList = mPreferences.getString(PREF_PLAYERS_LIST, null);
+        Gson gson = new Gson();
+        ArrayList playersList = gson.fromJson(jsonPlayersList, ArrayList.class);
+
+        /*String string1 = "Hello ";
+        mTextView1.setText(string1);*/
+
+        setContentView(R.layout.activity_ranking);
+
         System.out.println("MainActivity::onStart()");
-
     }
-
-    private void displayRanking() {
-
-}
 }
 
 
